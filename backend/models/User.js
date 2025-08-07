8a0a4656472c5c46da4d44ac7c6e466168d3fc15
agbibrightlucien <agbibrightlucienjnr@gmail.com>
2025-08-07 22:21:10 +0000

    Refactor User model to use configuration for bcrypt salt rounds and reset token expiry; add lastLogin field and update methods for better user management

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const { config } = require('../config/environment');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,7 +30,12 @@ const userSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  },
+  lastLogin: {
+    type: Date
   }
+}, {
+  timestamps: true // Adds createdAt and updatedAt automatically
 });
 
 // Hash password before saving
@@ -36,7 +43,7 @@ userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
   try {
-    const salt = await bcrypt.genSalt(12);
+    const salt = await bcrypt.genSalt(config.BCRYPT_SALT_ROUNDS);
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (error) {
@@ -49,12 +56,28 @@ userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
+// Update last login time
+userSchema.methods.updateLastLogin = function() {
+  this.lastLogin = new Date();
+  return this.save();
+};
+
 // Generate password reset token
 userSchema.methods.generatePasswordResetToken = function() {
-  const resetToken = require('crypto').randomBytes(32).toString('hex');
-  this.resetPasswordToken = require('crypto').createHash('sha256').update(resetToken).digest('hex');
-  this.resetPasswordExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+  const resetToken = crypto.randomBytes(32).toString('hex');
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  // Token expires using config value
+  this.resetPasswordExpires = Date.now() + config.RESET_TOKEN_EXPIRY_MINUTES * 60 * 1000;
   return resetToken;
+};
+
+// Static method to find user by reset token
+userSchema.statics.findByResetToken = function(token) {
+  const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+  return this.findOne({
+    resetPasswordToken: hashedToken,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
 };
 
 module.exports = mongoose.model('User', userSchema);
