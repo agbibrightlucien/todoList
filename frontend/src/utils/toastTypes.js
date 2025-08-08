72c5d41187851351bcc2d10f576092571fc84fa4
agbibrightlucien <agbibrightlucienjnr@gmail.com>
2025-08-08 05:34:57 +0000

    Add toastTypes utility for standardized toast notification types

---
+++ frontend/src/utils/toastTypes.js
@@ -0,0 +1,12 @@
+/**
+ * Toast notification types and utilities
+ */
+
+export const TOAST_TYPES = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+  INFO: 'info',
+  WARNING: 'warning',
+};
+
+export default TOAST_TYPES;
